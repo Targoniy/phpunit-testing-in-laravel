45a03babb9c4e172688a98ddf48594968488e937
Andriy Targoniy <9225@ukr.net>
2017-05-01 22:06:33 +0300

    First commit

---
+++ tests/unit/ProductTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Product;
+
+class ProductTest extends PHPUnit_Framework_TestCase
+{
+
+    protected $product;
+
+    public function setUp()
+    {
+        $this->product = new Product('Fall 4', 59); 
+    }
+    /** @test */    
+    function a_product_has_a_name()
+    {
+        $this->assertEquals('Fall 4', $this->product->name());
+    }
+    /** @test */
+    public function a_product_has_a_cost()
+    {
+        $this->assertEquals(59, $this->product->cost());        
+    }
+}
